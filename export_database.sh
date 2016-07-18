1c306f0f5f1d6dad376845405e8babc60fe52343
Ethan Turner <ezturner@washington.edu>
2016-07-18 15:09:15 -0700

    Imporved export_database.sh by removing changes to .my.cnf

--- export_database.sh
+++ export_database.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 PASSWORD=`cat /vagrant/provisioning/generated/mysqlrt`
-echo -e "[mysqldump]\npassword=" >> ~/my.cnf
-echo $PASSWORD >> ~/my.cnf
 mysqldump -u hrp --password=${PASSWORD} --all-databases > hrp_portal.sql
